e2ba9742bca090aa10534557c99071018878432c
Shulun Lyu <lyu.shul@northeastern.edu>
2023-01-14 16:23:32 -0800

    First commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// Use the .env file here
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+// Config the dotenv
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL = 'mongodb+srv://sllv:sllv123@cluster0.sc8tvmc.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+// Use mongoose to connect to the database
+// Change the URL to process.env
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set( 'useFindAndModify', false );
